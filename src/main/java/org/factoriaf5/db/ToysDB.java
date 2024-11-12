caec5b931f3d644530fb101b6df58ecaa1bf8d8e
olegukr <oleg.poberezhets@gmail.com>
2024-11-12 12:52:16 +0100

    created: classes MVC

---
+++ src/main/java/org/factoriaf5/db/ToysDB.java
@@ -0,0 +1,12 @@
+package org.factoriaf5.db;
+
+import java.util.List;
+
+import org.factoriaf5.models.BadToy;
+import org.factoriaf5.models.GoodToy;
+
+public class ToysDB {
+    private List<GoodToy> goodToys;
+    private List<BadToy> badTous;
+
+}
---
+++ src/main/java/org/factoriaf5/models/GoodToy.java
@@ -0,0 +1,5 @@
+package org.factoriaf5.models;
+
+public class GoodToy {
+
+}
