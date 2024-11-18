ce0ee5995f7fae8a4bdf231d08261509bedb9944
olegukr <oleg.poberezhets@gmail.com>
2024-11-18 22:21:12 +0100

    SCTF-52: created Toy GoodToy BadToy classes

--- src/main/java/org/factoriaf5/models/BadToy.java
+++ src/main/java/org/factoriaf5/models/BadToy.java
@@ -1,5 +1,25 @@
 package org.factoriaf5.models;
 
-public class BadToy {
+public class BadToy extends Toy {
+
+    private String content;
+
+    public BadToy(String title, boolean isGoodToy, String content) {
+        super(title, isGoodToy);
+        this.content = content;
+    }
+
+    public BadToy(String id, String title, boolean isGoodToy, String content) {
+        super(id, title, isGoodToy);
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 
 }
--- src/main/java/org/factoriaf5/models/GoodToy.java
+++ src/main/java/org/factoriaf5/models/GoodToy.java
@@ -1,5 +1,48 @@
 package org.factoriaf5.models;
 
-public class GoodToy {
+public class GoodToy extends Toy {
+
+    private String brand;
+    private int targetAge;
+    private String category;
+
+    public GoodToy(String title, boolean isGoodToy, String brand, int targetAge, String category) {
+        super(title, isGoodToy);
+        this.brand = brand;
+        this.targetAge = targetAge;
+        this.category = category;
+    }
+
+    public GoodToy(String id, String title, boolean isGoodToy, String brand, int targetAge, String category) {
+        super(id, title, isGoodToy);
+        this.brand = brand;
+        this.targetAge = targetAge;
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getTargetAge() {
+        return targetAge;
+    }
+
+    public void setTargetAge(int targetAge) {
+        this.targetAge = targetAge;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
 }
+
